5ae8c4122dac0629f2629c0189864d355e897dab
omar <omar.y.altawil@gmail.com>
2019-02-10 22:39:49 +0400

    dataColaction

---
+++ bme280/bme280.ino
@@ -0,0 +1,58 @@
+
+#include <stdint.h>
+#include "SparkFunBME280.h"
+#include "Wire.h"
+
+BME280 mySensor;
+
+void setup()
+{
+	mySensor.settings.commInterface = I2C_MODE;
+	mySensor.settings.I2CAddress = 0x77;
+	
+
+	mySensor.settings.runMode = 3; //Normal mode
+
+	mySensor.settings.tStandby = 0;
+	
+
+	mySensor.settings.filter = 0;
+
+	mySensor.settings.tempOverSample = 1;
+
+  mySensor.settings.pressOverSample = 1;
+	
+	mySensor.settings.humidOverSample = 1;
+	
+	Serial.begin(57600);
+	
+	delay(10); 
+	Serial.println(mySensor.begin(), HEX);
+
+}
+
+void loop()
+{
+	
+	Serial.print("Temperature: ");
+	Serial.print(mySensor.readTempC(), 2);
+	Serial.println(" degrees C");
+
+	Serial.print("Pressure: ");
+	Serial.print(mySensor.readFloatPressure(), 2);
+	Serial.println(" Pa");
+
+	Serial.print("Altitude: ");
+	Serial.print(mySensor.readFloatAltitudeMeters(), 2);
+	Serial.println("m");
+
+
+	Serial.print("Humidity: ");
+	Serial.print(mySensor.readFloatHumidity(), 2);
+	Serial.println(" %");
+	
+	Serial.println();
+	
+	delay(10000);
+
+}
---
+++ collacting_data/collacting_data.ino
@@ -0,0 +1,56 @@
+
+//we include I2S(standard used for connecting digital audio devices togethe)
+#include <I2S.h>
+void setup() {
+  //we set the Serial to 115200 baud
+  Serial.begin(115200);
+  //we enable communication and pass (mode, sampleRate, bitsPerSample)
+  I2S.begin(I2S_PHILIPS_MODE, 17000, 32);
+  //we initialize pin A0(button) as Input
+  pinMode(A0,INPUT);
+
+}
+ int i;
+void loop() {
+    //check if the button pressed
+    if(digitalRead(A0)){
+       //we start reading the value from the sensors
+       int sample = I2S.read();
+       //we filter 0 and -1 from the collected samples
+       if ((sample == 0) || (sample == -1) ) {return;}
+       // convert to 18 bit signed
+       sample >>= 14;
+       //we print the sample so the pySerial can read it
+       Serial.println(sample);
+       //increment i(i is number of sample capture)
+        i++;
+    //check if number of sample capture is 255
+       if(i%255==254){
+        //if the first swith is ON
+         if(!digitalRead(A1)){
+            //send label as 0 (water OFF)
+            Serial.println(0);
+            //wait 2S befor capture other data
+            delay(2000);
+         }
+         //if the first swith is OFF
+         else{
+         //if the second switch is ON
+         if(! digitalRead(A2)){
+            //send label as 1 (water half ON)
+            Serial.println(1);
+            //wait 2S befor capture other data
+            delay(2000);
+         }
+         //if the second switch is OFF
+         else{
+            //send label as 2 (water full ON)
+            Serial.println(2);
+            //wait 2S befor capture other data
+            delay(2000);
+         }
+         }
+       }
+         
+    }
+}
