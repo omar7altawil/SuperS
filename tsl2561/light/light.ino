7f054312c86266500cae3802eb8940e2f6223db7
omar <omar.y.altawil@gmail.com>
2019-02-10 22:38:39 +0400

    lightS

---
+++ tsl2561/light/light.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+
+void setup(void) 
+{
+  Serial.begin(9600);
+ 
+}
+
+
+void loop(void) 
+{  
+  sensors_event_t event;
+  tsl.getEvent(&event);
+  Serial.println(event.light);
+
+}
